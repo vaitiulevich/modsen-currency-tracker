0acc679ed959bd6e824063ea9dc93b362f350876
HP <vai.katia03@mail.ru>
2024-09-18 23:04:20 +0300

    build: add key api

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -9,7 +9,7 @@ export const endPoints = {
 export const FETCH_INTERVAL = 25000000;
 export const COUNT_FETCH_DAYS = 30;
 
-export const API_KEY = 'cur_live_QQoYQScj9JjCrJcNXuZCMPw6FFlKh2vEuG4e22Tm';
+export const API_KEY = 'cur_live_7stknQCtmvxl34jCS42E7YtnBS1vsF1nvGXA35QM';
 export const COIN_API_KEY = '910C1E7C-062B-49F4-A48B-F3D82043D025';
 export const MAPBOX_TOKEN =
   'pk.eyJ1Ijoia2F0ZWthdGUwMyIsImEiOiJjbTBxbzdxZGQwMDVnMmlzYmYzOWY2bXVzIn0.gHTo5KsWLetooYvXZK0tsw';
